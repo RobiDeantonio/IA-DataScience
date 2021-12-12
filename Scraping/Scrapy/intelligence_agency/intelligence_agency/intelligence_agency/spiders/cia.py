e4d09d4fb00c312d93c87d7835e25c11c38a0d34
RobiDeantonio <rysanchezd@unal.edu.co>
2021-12-12 12:59:34 -0500

    Submodule Intelligence Agency Website

---
+++ Scraping/Scrapy/intelligence_agency/intelligence_agency/intelligence_agency/spiders/cia.py
@@ -0,0 +1,33 @@
+import scrapy
+
+## links: //a[starts-with(@href,"collection") and (parent::h3 | parent::h2)]/@href
+## title: //h1[@class="documentFirstHeading"]/text()  
+## body: //div[@class="field-item even"]//p[not(@class)]/text()'
+
+class SpyderCIA(scrapy.Spider):
+    name = 'cia'
+    start_urls = [
+        'https://www.cia.gov/readingroom/historical-collections'
+    ]
+
+    custom_settings = {
+        'FEED_URI': 'cia.json',
+        'FEED_FORMAT': 'json',
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    }
+
+    def parse(self, response):
+        links_desclassified = response.xpath('//a[starts-with(@href,"collection") and (parent::h3|parent::h2)]/@href').getall()
+        for link in links_desclassified:
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
+
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]//p[not(@class)]/text()').get()
+
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph
+        }
